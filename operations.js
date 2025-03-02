e8213948dfeb6cbcaa851e0291bdfcbeef7a200f
Leo Kim <imleokkkk@naver.com>
2025-03-02 21:21:29 +0900

    mq 4-2 created

---
+++ operations.js
@@ -0,0 +1,10 @@
+const add = (val1, val2) => val1 + val2;
+const subtract = (val1, val2) => val1 - val2;
+
+export { add, subtract };
+
+/*
+export const add = ...
+export const subtract = ...
+도 가능하다. 
+*/
